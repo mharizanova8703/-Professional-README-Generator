21317aa940c06f3585302298ea998b513dc4d9f2
mharizanova8703 <mharizanova@yahoo.com>
2021-10-22 22:07:59 -0400

    updating index.js
    adding promp questions

---
+++ Develop/index.js
@@ -0,0 +1,80 @@
+let inquirer = require('inquirer');
+let fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description for your project:?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions step by step (split by comma, no space)',
+    },
+     {
+      type: 'input',
+      name: 'test',
+      message: 'Is there a test included?',
+    },
+
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information for your project:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter contribution guidelines for your project:',
+    },
+     {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+    {
+      type: 'input',
+      name: 'GitHub',
+      message: 'Enter your GitHub Username:',
+    },
+     
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+    {
+      type: 'list',
+      name: 'licence',
+      message: 'Chose the appropriate license for this project:',
+       choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+    
+  ])
+
+// TODO: Create an array of questions for user input
+const questions = []
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init()
